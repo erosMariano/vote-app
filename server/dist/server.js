4bea3c4b728491b7afeb3393a8c22e50064b7164
erosMariano <erosmariano1@gmail.com>
2022-05-25 19:22:15 -0300

    att

---
+++ server/dist/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const votes_routes_1 = require("./routes/votes.routes");
+const community_routes_1 = require("./routes/community.routes");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+//Config rotas
+app.use(votes_routes_1.votesRoute);
+app.use(community_routes_1.communityRoute);
+app.listen(process.env.PORT || 3333, () => {
+    console.log("HTTP server running");
+});
